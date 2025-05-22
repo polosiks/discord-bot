a8019b5e125bd3502bac17f385a77f7c6811bcf9
polosiks <gytisbutni@gmail.com>
2025-05-22 20:36:38 +0300

    Initial clean commit without secrets

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    for emoji in ['🇰', '🇾', '🇸']:
+        await message.add_reaction(emoji)
+
+client.run(TOKEN)
